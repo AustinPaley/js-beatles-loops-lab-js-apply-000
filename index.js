ad0075eeceec70fce67008e8191ac84eed6e0377
Austin Paley <austinpaley32@gmail.com>
2018-02-22 16:49:44 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+function johnLennonFacts (){
+var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+var newfacts = [];
+var i = 0
+while (i<facts.length) {
+  newfacts.push(facts[i] + "!!!")
+  i++;
+}
+return newfacts
+}
+
+function iLoveTheBeatles (number){
+  var number=[];
+  do {
+    
+  }
+  "I love the Beatles!"
+  number < 15
+  while (i < 15);
+}
