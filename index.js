51923dd03c562b3af003ff64e6568f24abe481cb
Austin Paley <austinpaley32@gmail.com>
2018-02-22 17:05:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function johnLennonFacts (){
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
 var newfacts = [];
 var i = 0
-while (i < facts.length) {
+while (newfacts.length < facts.length) {
   newfacts.push(facts[i] + "!!!");
   i++;
 }
