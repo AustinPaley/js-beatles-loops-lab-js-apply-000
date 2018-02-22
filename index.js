c232ae03e11b60f9a28a88df7a00a48bd02eb52b
Austin Paley <austinpaley32@gmail.com>
2018-02-22 17:12:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function johnLennonFacts (facts){
 var newfacts = [];
 var i = 0;
 while (i < facts.length) {
-  newfacts.push('${facts[i]}!!!');
+  newfacts.push(`${facts[i]}!!!`);
   i++;
 }
 return newfacts;
