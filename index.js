8e30f022bde00b372a286048c1b9e8e14e02f630
Austin Paley <austinpaley32@gmail.com>
2018-02-22 16:54:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function theBeatlesPlay(musicians, instruments){
   var array = [];
-  for
+  for(){
+    
+  }
 }
 function johnLennonFacts (){
 var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
